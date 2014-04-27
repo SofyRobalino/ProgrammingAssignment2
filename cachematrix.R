b62123fec06086ca86c07ad27d681b84b8399f7e
SofyRobalino <sarobalino@espe.edu.ec>
2014-04-27 13:15:12 -0500

    inverse matrix
    this code allow get a matrix and present the inverse of the got matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+i  <- NULL
+        st  <- function(y){
+                x <<- a
+                i <<- NULL 
+        }
+        gt  <- function() x
+        stinverse  <- function(inverse) i  <<- inverse
+        gtinverse  <- function() i
+        list(st= st, gt = gt, 
+             stinverse = stinverse, 
+             gtinverse = gtinverse)
 
 }
 
@@ -11,5 +22,19 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+ i  <- x$gtinverse()
+        if (!is.null(i)){
+                message("gtting cached data")
+                return(i)
+        }
+        data  <- x$gt()
+        i  <- solve(data, ...)
+        x$stinverse(i)
+        i
+
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+
+
